f242007d5319ca97cf33aa62afe357f5601a1b4e
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2023-10-10 10:56:36 +0200

    Toevoeging BootstrapMade template "The event"

---
+++ Mijn CV/Mijn CV/Controllers/CharacterProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mijn_CV.Controllers
+{
+    public class CharacterProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mijn CV/Mijn CV/Controllers/OrganisationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mijn_CV.Controllers
+{
+    public class OrganisationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
