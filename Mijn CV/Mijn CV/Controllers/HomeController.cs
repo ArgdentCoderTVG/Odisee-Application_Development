bce83c3bdb0933182c71985633e5f54603f080f7
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2023-10-10 11:49:33 +0200

    Transformed HTML template to CSHTML view

--- Mijn CV/Mijn CV/Controllers/HomeController.cs
+++ Mijn CV/Mijn CV/Controllers/HomeController.cs
@@ -18,7 +18,12 @@ namespace Mijn_CV.Controllers
 			return View();
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult IndexTheEvent()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
