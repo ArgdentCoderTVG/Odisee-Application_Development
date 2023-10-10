195bd57c19abfd0b5a3447f2da42e2b3a4855c75
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2023-10-10 23:29:55 +0200

    Done!

--- Mijn CV/Mijn CV/wwwroot/js/myCustomCode.js
+++ Mijn CV/Mijn CV/wwwroot/js/myCustomCode.js
@@ -1,6 +1,5 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         document.getElementById("navBar").classList.add("show");
-        console.log("TESSSST");
     }, 10000);
 });
