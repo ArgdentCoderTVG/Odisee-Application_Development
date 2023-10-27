f2812744e671625953fec0e7429f906bcce5f8cb
Tibo Van Gindertaelen <46933283+TVeegy@users.noreply.github.com>
2023-10-27 15:47:16 +0200

    (not done) Index UI design -> Developing GreetingText class + UI implementation

--- 1.0-assigments/Comme_Chez_Swa/Controllers/HomeController.cs
+++ 1.0-assigments/Comme_Chez_Swa/Controllers/HomeController.cs
@@ -2,12 +2,21 @@
 using Comme_Chez_Swa.Models.Home;
 using Comme_Chez_Swa.Models.Home.Menu;
 using Comme_Chez_Swa.Models.Home.Reservations;
+using Comme_Chez_Swa.Models.Home.Utility;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 
 namespace Comme_Chez_Swa.Controllers
 {
+    public enum MenuTypes /* TODO: Is this the correct name(convention) using ...Types? */
+    {
+        Ontbijt,
+        Lunch,
+        Suggestie
+    }
     public class HomeController : Controller
 	{
         // [ESSENCE] instance backing variable
@@ -23,15 +32,49 @@ namespace Comme_Chez_Swa.Controllers
         public IActionResult Index()
 		{
             // Arrange data
-            string dynamicGreetingText = String.Empty; 
-            string dynamicMenuText = String.Empty; 
-            string restaurantLogoSource = String.Empty;
+            string dynamicGreetingText = "Greeting placeholder";
+            MenuTextWithPageLinkMVC dynamicMenuText = GenerateDynamicMenuText(); 
+            string restaurantLogoSource = "/img/logo_comme_chez_swa.png";
 
             IndexViewModel indexViewModel = new IndexViewModel(dynamicGreetingText, dynamicMenuText, restaurantLogoSource);
 
             // Act
-            return View();
+            return View(indexViewModel);
 		}
+
+        private MenuTextWithPageLinkMVC GenerateDynamicMenuText()
+        {
+            // TODO: Implicit casting, but I actually dont know when to use implicit/explicit
+            byte currentHour = (byte)DateTime.Now.Hour;
+
+            MenuTypes menuType = default;
+
+            Func<int, bool> isGivenHourBefore11AM = (funcCurrentHour) => { return (funcCurrentHour > 11); };
+            Func<int, bool> isGivenHourBetween11AM_NotIncluded2PM = (funcCurrentHour) => { return (funcCurrentHour >= 11 && funcCurrentHour < 14); };
+            Func<int, bool> isGivenHourBetween2PM_NotIncluded5PM = (funcCurrentHour) => { return (funcCurrentHour >= 14 && funcCurrentHour < 17); };
+            Func<int, bool> isGivenHourAfter5PM = (funcCurrentHour) => { return (funcCurrentHour >= 17); };
+
+            // TODO: This feels like a switch statement, but cant be done using a switch?
+            if (isGivenHourBefore11AM(currentHour))
+            {
+                menuType = MenuTypes.Ontbijt;
+            }
+            else if (isGivenHourBetween11AM_NotIncluded2PM(currentHour))
+            {
+                menuType = MenuTypes.Lunch;
+            }
+            else if (isGivenHourBetween2PM_NotIncluded5PM(currentHour))
+            {
+                menuType = MenuTypes.Suggestie;
+            }
+            else if (isGivenHourAfter5PM(currentHour))
+            {
+                menuType = MenuTypes.Suggestie;
+            }
+
+            // TODO: I chose the {0} syntax, I need something universally thats not subjective.
+            return new MenuTextWithPageLinkMVC("Neem snel een kijkje naar ons heerlijke {0}-menu!","{0}", menuType.ToString(), "Menu", "Home");
+        }
 
 		// [ESSENCE] Home controller About action 
 		public IActionResult About()
---
+++ 1.0-assigments/Comme_Chez_Swa/Models/Home/Utility/MenuTextWithPageLinkMVC.cs
@@ -0,0 +1,22 @@
+﻿namespace Comme_Chez_Swa.Models.Home.Utility
+{
+    // [NOTE] POCO with data integrity focus, solely instanced/manipulated using constructor (instance state is always intial state)
+    public class MenuTextWithPageLinkMVC
+    {
+        public string FullText { get; }
+        public string LinkedText { get; }
+        public string LinkedTextPlaceholder { get; }
+        public string NameOfActionMVC { get; }
+        public string NameOfControllerMVC { get; }
+
+        // [NOTE] One entry point for altering the object state
+        public MenuTextWithPageLinkMVC(string fullText, string linkedText, string linkedTextPlaceholder, string nameOfActionMVC, string nameOfControllerMVC)
+        {
+            FullText = fullText;
+            LinkedText = linkedText;
+            LinkedTextPlaceholder = linkedTextPlaceholder;
+            NameOfActionMVC = nameOfActionMVC;
+            NameOfControllerMVC = nameOfControllerMVC;
+        }
+    }
+}
